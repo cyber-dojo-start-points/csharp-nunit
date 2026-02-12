246e1e3e41494a46b53f4e5336dbfd5c9d0e9a39
JonJagger <jon@kosli.com>
2026-02-12 10:31:28 +0000

    Update target image in manifest

--- start_point/cyber-dojo.sh
+++ start_point/cyber-dojo.sh
@@ -1,2 +1,22 @@
+# --------------------------------------------------------------
+# Text files under /sandbox are automatically returned...
+source ~/cyber_dojo_fs_cleaners.sh
+
+function cyber_dojo_enter()
+{
+  : # 1. Only return _newly_ generated reports.
+  #cyber_dojo_reset_dirs ${...}
+}
+function cyber_dojo_exit()
+{
+  : # 2. Remove text files we don't want returned.
+  cyber_dojo_delete_dirs /sandbox/dojo/bin 
+  cyber_dojo_delete_dirs /sandbox/dojo/obj
+  #cyber_dojo_delete_files ...
+}
+cyber_dojo_enter
+trap cyber_dojo_exit EXIT SIGTERM
+
 cd dojo
-dotnet test
+dotnet restore --source /home/sandbox/.nuget/packages/
+dotnet test --no-restore
