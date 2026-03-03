b8e59907eb916dc57ed18279147ba3448e506085
Martin Klose <109064+mklose@users.noreply.github.com>
2026-03-03 17:11:05 +0100

    fix test result matcher

--- start_point/red_amber_green.rb
+++ start_point/red_amber_green.rb
@@ -1,8 +1,12 @@
 
 lambda { |stdout,stderr,status|
   output = stdout + stderr
+   #dotnet test:
   return :green if /^Passed!/.match(output)
-  return :red   if /^Failed!/.match(output) #dotnet test
-  return :red   if /^\s+Failed Tests/.match(output) #nunit console runner 
+  return :red   if /^Failed!/.match(output)
+
+  #nunit console runner:
+  return :green if /^\s+Overall result: Passed/.match(output)
+  return :red   if /^\s+Failed Tests/.match(output) 
   return :amber
 }
