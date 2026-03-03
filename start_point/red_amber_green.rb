9d1d1797cc35b0af355f3cde992a103690fd7c27
Martin Klose <109064+mklose@users.noreply.github.com>
2026-03-03 17:07:24 +0100

    find failed tests for dotnet and nunit runner

--- start_point/red_amber_green.rb
+++ start_point/red_amber_green.rb
@@ -2,6 +2,7 @@
 lambda { |stdout,stderr,status|
   output = stdout + stderr
   return :green if /^Passed!/.match(output)
-  return :red   if /^Failed!/.match(output)
+  return :red   if /^Failed!/.match(output) #dotnet test
+  return :red   if /^\s+Failed Tests/.match(output) #nunit console runner 
   return :amber
 }
